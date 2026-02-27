794b5b39280581060fdc774eaed88e284527eeb8
Toqeer Awan <toqeerm250@gmail.com>
2026-02-27 19:58:17 +0500

    swagger

--- backend/config/swagger.js
+++ backend/config/swagger.js
@@ -24,7 +24,7 @@ Authorization: Bearer <your-jwt-token>
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
+        url: 'http://app.cloudverse.space/api',
         description: 'Development server'
       }
     ],
